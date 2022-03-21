8699ac1c5ae9f866b5621b9c65a1c26b3e5e4689
l3miage-MBE <mahrezmus@gmail.com>
2022-03-21 18:06:35 +0100

    Mise à jour

--- eclipse-workspace/bd/src/main/java/groupe7/bd/model/Bornette.java
+++ eclipse-workspace/bd/src/main/java/groupe7/bd/model/Bornette.java
@@ -4,5 +4,27 @@ import groupe7.bd.*;
 public class Bornette {	
 
 	Interface inter;//Permet d'accer au fonction de l'interface
+	private int idBornette;
+	private Etat etatBornette;
+
+	public Bornette (int idBornette,Etat etatBornette){
+          this.idBornette=idBornette;
+		  this.etatBornette=etatBornette;
+	}
+
+	//getters et setters
+    public int getIdBornette(){
+		return idBornette;
+	}
+	public void setIdBornette(int idBornette){
+		this.idBornette=idBornette;
+	}
+
+	public Etat getEtatBornette(){
+		return etatBornette;
+	}
+	public void setEtatBornette(Etat etatBornette){
+		this.etatBornette=etatBornette;
+	}
 
 }
