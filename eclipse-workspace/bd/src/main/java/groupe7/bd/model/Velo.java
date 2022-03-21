91aa958812229fad68af11ae0ff9f16dfe7d843a
l3miage-duongti <khoiduong12@gmail.com>
2022-03-21 14:55:52 +0100

    Mise a jour Velo.java

--- eclipse-workspace/bd/src/main/java/groupe7/bd/model/Velo.java
+++ eclipse-workspace/bd/src/main/java/groupe7/bd/model/Velo.java
@@ -1,7 +1,53 @@
 package groupe7.bd.model;
 
+import java.sql.Date;
+
 import groupe7.bd.*;
 
 public class Velo {
+
 	Interface inter;//Permet d'accer au fonction de l'interface
+
+	private int idVelo;
+	private Model modeleVelo;
+	private Etat etat;
+	private Date dateMeS;
+
+	//Constructeur
+	public Velo(int idVelo, Model modeleVelo, Etat etat, Date dateMeS) {
+		this.idVelo = idVelo;
+		this.modeleVelo = modeleVelo;
+		this.etat = etat;
+		this.dateMeS = dateMeS;
+	}
+
+	//getters et setters
+	public int getIdVelo() {
+		return idVelo;
+	}
+
+	public void setIdVelo(int idVelo) {
+		this.idVelo = idVelo;
+	}
+
+	public Model getModeleVelo() {
+		return modeleVelo;
+	}
+
+	public void setModeleVelo(Model modeleVelo) {
+		this.modeleVelo = modeleVelo;
+	}
+
+	public void setEtat(Etat etat) {
+		this.etat = etat;
+	}
+
+	public Date getDateMeS() {
+		return dateMeS;
+	}
+
+	public void setDateMeS(Date dateMeS) {
+		this.dateMeS = dateMeS;
+	}
+
 }
